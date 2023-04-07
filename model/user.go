feddfa4572518537472e41da947a4f01cfe401dc
贞贞 <159420113@qq.com>
2023-04-08 05:23:26 +0800

    debug

--- model/user.go
+++ model/user.go
@@ -6,26 +6,10 @@ import (
 
 type User struct {
 	gorm.Model
-	name    string  `gorm:"type:varchar(32);not null;unique;comment:用户名"`
-	balance float64 `gorm:"type:decimal(10,2);default:0;comment:余额"`
+	Name    string  `gorm:"type:varchar(32);not null;unique;comment:用户名"`
+	Balance float64 `gorm:"type:decimal(10,2);default:0;comment:余额"`
 }
 
 func (u *User) AddBalance(amount float64) {
-	u.balance += amount
-}
-
-func (u *User) Balance() float64 {
-	return u.balance
-}
-
-func (u *User) SetBalance(balance float64) {
-	u.balance = balance
-}
-
-func (u *User) Name() string {
-	return u.name
-}
-
-func (u *User) SetName(name string) {
-	u.name = name
+	u.Balance += amount
 }
